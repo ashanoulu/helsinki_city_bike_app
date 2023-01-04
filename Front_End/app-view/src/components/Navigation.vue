7bf8946b4c85adf08384dd1d95ef2a247a25fb2f
Ashan Chameera <awalpita20@student.oulu.fi>
2023-01-04 19:35:58 +0200

    update Docs :memo:

--- Front_End/app-view/src/components/Navigation.vue
+++ Front_End/app-view/src/components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Helsinki Bike App</a>
+      <a class="navbar-brand" href="#">Helsinki City Bike App</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
               aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
